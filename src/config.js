c7ae0aa803d12e7f161b34d4a3e393cdd37dcc62
LasaleFamine <info@godev.space>
2016-10-01 21:46:47 +0200

    resitfy

--- src/config.js
+++ src/config.js
@@ -2,27 +2,27 @@
 
 //  require('dotenv').config()
 const builder = require('botbuilder')
-// const restify = require('restify')
+const restify = require('restify')
 
 //  =========================================================
 //  Bot Setup
 //  =========================================================
 
 // Setup Restify Server
-// const server = restify.createServer()
-// server.listen(process.env.port || process.env.PORT || 8080, () => {
-//   console.log(`${server.name} listening to ${server.url}`)
-// })
-//
-// const connector = new builder.ChatConnector({
-//   appId: process.env.MICROSOFT_APP_ID,
-//   appPassword: process.env.MICROSOFT_APP_PASSWORD
-// })
+const server = restify.createServer()
+server.listen(process.env.port || process.env.PORT || 8080, () => {
+  console.log(`${server.name} listening to ${server.url}`)
+})
 
-const connector = new builder.ConsoleConnector().listen()
+const connector = new builder.ChatConnector({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD
+})
+
+// const connector = new builder.ConsoleConnector().listen()
 
 const bot = new builder.UniversalBot(connector)
 
-// server.post('/api/messages', connector.listen())
+server.post('/api/messages', connector.listen())
 
 module.exports = {builder, bot}
